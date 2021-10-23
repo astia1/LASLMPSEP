5c65fd1e0194898a71a320406bc3f37bc6d6d4f4
John McKinney <john@mckinneyphoto.com>
2021-10-23 01:02:33 +0000

    notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $("#covidButton").click(function(){
         $('#covidModal').modal('show')
     });
-    
+
+// upper map pin    
 $("#mapButton").click(function(){
         $('#mapModal').modal('show')
     });
     
-    
+// lower map pin
 $("#mapButton2").click(function(){
         $('#mapModal').modal('show')
     });
